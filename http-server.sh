103bb105fdc7b0dedcd87cab2173f82711351673
Jess Thrysoee <jess@thrysoee.dk>
2020-05-14 15:09:10 +0200

    Add PORT parameter to http-server.sh

--- http-server.sh
+++ http-server.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+PORT=${1:-2000}
+
 if [[ $(python --version 2>&1) == Python\ 2* ]]
 then
-  python -m SimpleHTTPServer 2000
+  python -m SimpleHTTPServer $PORT
 else
-  python -m http.server 2000
+  python -m http.server $PORT
 fi
 
